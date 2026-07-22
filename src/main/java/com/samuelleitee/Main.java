cd78597e37279667e7e7d98652a11aa0506f69fa
samuelleitee <programmer.and.investor@gmail.com>
2026-07-22 11:28:04 -0300

    initializing project with IntelliJ

---
+++ src/main/java/com/samuelleitee/Main.java
@@ -0,0 +1,12 @@
+package com.samuelleitee;
+
+public class Main {
+    static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        String message = "Programando em ";
+
+        System.out.println(message + args[0]);
+        System.out.println("Precisamos de ajuda!");
+    }
+}
